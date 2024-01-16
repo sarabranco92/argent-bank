f394a5f5e0e7bbdf27a43b68b58bc4e5c8d4845e
sarabranco92 <sara1992@live.com.pt>
2024-01-16 14:22:18 +0100

    Bug in Git pages: BrowserRoute to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from '../src/redux/store'; 
@@ -9,9 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
+            <HashRouter>
                 <App />
-            </BrowserRouter>
+            </HashRouter>
         </Provider>
     </React.StrictMode>
 );
