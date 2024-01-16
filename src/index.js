0818bae259219a736eeb272067c25269aad4491f
sarabranco92 <sara1992@live.com.pt>
2024-01-16 15:14:15 +0100

    git pages

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from '../src/redux/store'; 
@@ -8,9 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            
+            <HashRouter>
                 <App />
-  
+            </HashRouter>
         </Provider>
     </React.StrictMode>
 );
