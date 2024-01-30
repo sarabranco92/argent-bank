9b6c90c5579bf5bb8685694b96bccb5635f1cafb
sarabranco92 <sara1992@live.com.pt>
2024-01-30 11:31:30 +0100

    Final Project

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,14 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './reducers/authSlice'; 
 import userReducer from './reducers/userSlice'; 
 
-// Configuration du store Redux
 const store = configureStore({
   // Définition des reducers à inclure dans le store
   reducer: {
     auth: authReducer, // Reducer pour la gestion de l'authentification
     user: userReducer, // Reducer pour la gestion des données utilisateur
   },
-  // Ajout d'options supplémentaires au besoin (middleware, devTools, etc.)
 });
 
 export default store;
